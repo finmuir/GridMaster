484c6ade58ad10d47b4f0f344d184cd2cc59914f
sam4762 <147061431+sam4762@users.noreply.github.com>
2023-11-19 14:40:38 +0000

    Haversine calculations
    Changed all Euclidian distance calculations to Haversine distance calculations

--- MappingTest2/customer_clustering.py
+++ MappingTest2/customer_clustering.py
@@ -382,8 +382,24 @@ class CustomerClustering:
                 X_2 = cluster_2.position[0]
                 Y_2 = cluster_2.position[1]
 
+                # Radius of the Earth in meters
+                R = 6371000
+
+                # Converts latitude and longitude points  from degrees to radians
+                lat1 = math.radians(Y_1)
+                lon1 = math.radians(X_1)
+                lat2 = math.radians(Y_2)
+                lon2 = math.radians(X_2)
+
+                # Haversine formula calculates distance between two point on sphere
+                dlon = lon2 - lon1
+                dlat = lat2 - lat1
+                a = math.sin(dlat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2) ** 2
+                c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+                dist=R*c
                 # euclidian distance between nodes
-                dist = ((X_2 - X_1) ** 2 + (Y_2 - Y_1) ** 2) ** (1 / 2)
+                #dist = ((X_2 - X_1) ** 2 + (Y_2 - Y_1) ** 2) ** (1 / 2)
 
                 if self.max_distance != None and dist > self.max_distance:
 
--- MappingTest2/network_designer.py
+++ MappingTest2/network_designer.py
@@ -367,8 +367,24 @@ class NetworkDesigner:
                 x2 = node2.loc[0]
                 y2 = node2.loc[1]
 
+                # Radius of the Earth in meters
+                R = 6371000
+
+                # Converts latitude and longitude points  from degrees to radians
+                lat1 = math.radians(y1)
+                lon1 = math.radians(x1)
+                lat2 = math.radians(y2)
+                lon2 = math.radians(x2)
+
+                # Haversine formula calculates distance between two point on sphere
+                dlon = lon2 - lon1
+                dlat = lat2 - lat1
+                a = math.sin(dlat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2) ** 2
+                c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+                distance = R * c
+
                 # euclidean distance = sqrt((y2-y1)^2 - (x2-x1)^2)
-                distance = ((y2 - y1) ** 2 + (x2 - x1) ** 2) ** (1 / 2)
+               # distance = ((y2 - y1) ** 2 + (x2 - x1) ** 2) ** (1 / 2)
 
                 self.distances[i, j] = distance
 
