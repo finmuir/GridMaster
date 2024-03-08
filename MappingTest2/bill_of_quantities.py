99c5f9524ef1a6455a1a2332b5f8d273783e3e78
ChrisGibson03 <100776876+ChrisGibson03@users.noreply.github.com>
2024-03-08 14:05:33 +0000

    Update bill_of_quantities.py

--- MappingTest2/bill_of_quantities.py
+++ MappingTest2/bill_of_quantities.py
@@ -1,21 +1,21 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
 class BillOfQuantities:
-    def __init__(self, quantities_and_costs):
+    def __init__(self, quantities_and_costs, labor_cost, through_poles=0):
         self.quantities_and_costs = quantities_and_costs
+        self.labor_cost = labor_cost
+        self.through_poles = through_poles
 
     def generate_bill_of_quantities(self):
         bill_of_quantities = {}
-        for item, (quantity, unit_cost, labor_rate, installation_time) in self.quantities_and_costs.items():
-            total_cost = quantity * (unit_cost + labor_rate)
+        for item, (quantity, unit_cost, installation_time) in self.quantities_and_costs.items():
+            if item == "Bobbin Insulators":
+                # Adjusting quantity based on through poles
+                quantity += (2 * self.through_poles)
+            total_cost = (quantity * unit_cost) + (installation_time * self.labor_cost)
             bill_of_quantities[item] = {
                 "Quantity": quantity,
                 "Unit Cost": unit_cost,
-                "Labor Rate": labor_rate,
+                "Labor Cost": self.labor_cost,
                 "Installation Time (hrs)": installation_time,
                 "Total Cost": total_cost
             }
         return bill_of_quantities
-
